9da0ed6ce818bf1a440dbe2b0488b1ec7affd317
Joao Machado <jm@dockstreettrading.com>
2021-10-03 12:18:20 -0300

    Add migrations

---
+++ src/Simu.App/Data/Migrations/20211003143253_IncluindoModelos.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Simu.App.Data.Migrations
+{
+    public partial class IncluindoModelos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assuntos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoAssunto = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assuntos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Provas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Provas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questoes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProvaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AssuntoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Tema = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Conteudo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoAssunto = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Questoes_Assuntos_AssuntoId",
+                        column: x => x.AssuntoId,
+                        principalTable: "Assuntos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Questoes_Provas_ProvaId",
+                        column: x => x.ProvaId,
+                        principalTable: "Provas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questoes_AssuntoId",
+                table: "Questoes",
+                column: "AssuntoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questoes_ProvaId",
+                table: "Questoes",
+                column: "ProvaId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questoes");
+
+            migrationBuilder.DropTable(
+                name: "Assuntos");
+
+            migrationBuilder.DropTable(
+                name: "Provas");
+        }
+    }
+}
